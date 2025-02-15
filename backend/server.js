3bed60dc177d6370fc4756f64c15cafd446541ff
KanishkRichhariya107 <kanishkrichhariya10@gmail.com>
2025-02-15 19:55:49 +0530

    made the db connection with atlas server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 // npm install dotenv for env
 require('dotenv').config()
+//npm install mongoose
+const mongoose=require('mongoose');
 const express=require('express');
 
 const workoutRoutes=require('./routes/workout')
@@ -8,7 +10,7 @@ const app=express();
 
 //middlewear
 
-//when we are using post or patch request we are sending data through request object but for that to happen we need to use this.any request that comes in it looks if there is some data with the request adn if there is data it passes that data to the request object. we can access it in request handler by req.body  
+//when we are using post or patch request we are sending data through request object but for that to happen we need to use this.any request that comes in it looks if there is some data with the request and if there is data it passes that data to the request object. we can access it in request handler by req.body  
 app.use(express.json())
 
 app.use((req,res,next)=>{
@@ -22,7 +24,15 @@ app.use((req,res,next)=>{
 //this only works if this url is present in the start
 app.use('/api/workout',workoutRoutes)
 
+//connect to db and listen to server
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
 //listen for request
 app.listen(4000,()=>{
-    console.log("listening to port 4000")
+    console.log("listening to port",process.env.PORT);
 })
+})
+.catch((error)=>{
+    console.log(error);
+})
+
